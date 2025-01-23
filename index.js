c603b4b2d74b0d4a73d60a827bc03923b9596b45
Vaishnavi187 <vaishnavisingh2005.college@gmail.com>
2025-01-23 22:57:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const Router = require('./route/userroute');
  const routter = require('./route/contactroute');
 
 const app = express()
-app.use(cors())
+// app.use(cors())
+app.use(cors({ origin: 'https://mystore-fkq9.onrender.com/' }));
 
 const PORT=process.env.PORT|| 4001
 const URL=process.env.URI
